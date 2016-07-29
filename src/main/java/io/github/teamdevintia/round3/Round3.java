1592527029bedd7b8d68959c421ffb106d7c09db
MiniDigger <admin@minidigger.me>
2016-07-29 22:47:23 +0200

    initial commit, good luck guys, I will step into my plane now

---
+++ src/main/java/io/github/teamdevintia/round3/Round3.java
@@ -0,0 +1,18 @@
+package io.github.teamdevintia.round3;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Round3 extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
